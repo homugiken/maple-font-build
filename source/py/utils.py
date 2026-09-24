0f0ee90e0c767cb0b5bfaacc115878eedd8f9a93
homusuika <homugiken@gmail.com>
2026-09-24 12:15:55 +0800

    Update utils.py base_subfamily_list empty

--- source/py/utils.py
+++ source/py/utils.py
@@ -551,9 +551,10 @@ def parse_style_name(style_name_compact: str):
     #
     # same as `ftcli assistant commit . --ls 400 700`
     # https://github.com/ftCLI/FoundryTools-CLI/issues/166#issuecomment-2095756721
-    base_subfamily_list = ["Regular", "Bold", "Italic", "BoldItalic"]
+    # base_subfamily_list = ["Regular", "Bold", "Italic", "BoldItalic"]
+    base_subfamily_list = []
     if style_name_compact in base_subfamily_list:
-        return "", _style_name, _style_name, False, is_italic
+        return "", _style_name, _style_name, True, is_italic
     else:
         return (
             " " + style_name_compact.replace("Italic", ""),
